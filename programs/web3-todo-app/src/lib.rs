8b6283b8a092cd47b39bd3ef8ce426948e8887ec
Daniel-Tambee <danieltambee@gmail.com>
2024-05-15 17:03:23 +0000

    reorganized_exports

--- programs/web3-todo-app/src/lib.rs
+++ programs/web3-todo-app/src/lib.rs
@@ -6,9 +6,12 @@ declare_id!("9VELjY4xnvMWThciCAuMDZNiDYSCd3341BD7PmU1UxwC");
 
 #[program]
 pub mod web3_todo_app {
-    use super::*;
-    use controllers::{
-        todo::{ create_todo, find_todo_by_id, find_todo_by_wallet, find_todo_is_done },
-        user::{ create_user, find_user_by_id, find_user_todos },
+    pub use super::*;
+    pub use controllers::todo::{
+        create_todo,
+        find_todo_by_id,
+        find_todo_by_wallet,
+        find_todo_is_done,
     };
+    pub use controllers::user::{ create_user, find_user_by_id, find_user_todos };
 }
